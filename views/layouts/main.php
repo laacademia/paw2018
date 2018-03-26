3c9a3a982eda7728af990122b8ad466a8e10132c
laacademia <martingaray_12@hotmail.com>
2018-03-26 14:33:17 -0300

    css main layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
 	<title>Este es el layout principal</title>
 </head>
 <body>
-	<h1>Layout Principal</h1>
-	<section>
+	<header>
+		<h1>Layout Principal</h1>
+	</header>
+	<section id="main-container">
 		<h2>Aca le estamos metiendo el contenido de la vista que llama la accion del controlador</h2>
 		<article>
 			<?=$content?>			
